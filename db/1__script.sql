4578426a74542dcd84583a4da570c6c93998b9f2
Rodrigo Santos <rodrigo@improving.com.br>
2018-04-24 22:40:26 -0300

    Adicionado mapeamento de database

---
+++ db/1__script.sql
@@ -0,0 +1,7 @@
+create table produtos (id integer auto_increment primary key, nome varchar(255), preco decimal(10,2));
+alter table produtos add column usado boolean default false;
+alter table produtos add column descricao varchar(255);
+create table categorias (id integer auto_increment primary key, nome varchar(255));
+insert into categorias (nome) values ("Futebol"), ("Volei"), ("Tenis");
+alter table produtos add column categoria_id integer;
+update produtos set categoria_id = 1;
